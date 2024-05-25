1c92874d07f73312c87537ee36f04ca6526d39c8
nezar123456 <khodernezar@gmail.com>
2024-05-25 17:37:44 +0300

    lesson3

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname})
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+mongoose
+  .connect(
+    "mongodb+srv://nezar:z5ICY3aeuAV3Ti19@cluster0.zdagyii.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
